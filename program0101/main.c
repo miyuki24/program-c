8947b71e515d7f0c71216d89d134b9351ac097c3
miyuki24 <miyuki.0424@yahoo.ne.jp>
2021-01-30 16:59:16 +0900

    Initial Commit

---
+++ program0101/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  program0101
+//
+//  Created by 田中美幸 on 2021/01/30.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
